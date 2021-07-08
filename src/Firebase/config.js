f9b36f62d84ae87e394fa96671bb42c5e3b077f3
Pannu786 <thepannu786@gmail.com>
2021-07-08 14:55:03 +0530

    Set-up-database

---
+++ src/Firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyCzT3THIQkmm77UEzYCwhnMlb1orKR5k7I',
+  authDomain: 'first-fire-78e02.firebaseapp.com',
+  projectId: 'first-fire-78e02',
+  storageBucket: 'first-fire-78e02.appspot.com',
+  messagingSenderId: '861125312239',
+  appId: '1:861125312239:web:a50a4f18d20bd6972da51a',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
